6633b073ac23e9464d43a6391490d1e43ac2b496
krabitsch <krabitsch@users.noreply.github.com>
2026-05-05 15:08:05 +0200

    feat(class-service): added updateClass, deleteClass, getClassbyOrgID, updateAssignment, deleteAssignment, getAssignmentById and minor changes

--- src/packages/utils/src/classUtils.js
+++ src/packages/utils/src/classUtils.js
@@ -7,15 +7,26 @@ const getClasses = async(select = null)=>{
     return await prisma.class.findMany(options)
 }
 
-const getClassById = async(id, select = null, include = null)=>{
-    if (!id || isNaN(id))
+const getClassById = async(idClass, select = null, include = null)=>{
+    if (!idClass || isNaN(idClass))
         throw new Error('Invalid class ID');
-    const options = {where: {id: parseInt(id)}}
+    const options = {where: {id: parseInt(idClass)}}
     if(select)
         options.select = select
     if(include)
         options.include = include
     return await prisma.class.findUnique(options)
+}
+
+const getClassByOrgId = async(idOrg, select = null, include = null)=>{
+    if (!idOrg || isNaN(idOrg))
+        throw new Error('Invalid org ID');
+    const options = {where: {org_id: parseInt(idOrg)}}
+    if(select)
+        options.select = select
+    if(include)
+        options.include = include
+    return await prisma.class.findMany(options)
 }
 
 const getAssignments = async(classId, select = null)=>{
@@ -27,10 +38,10 @@ const getAssignments = async(classId, select = null)=>{
     return await prisma.assignment.findMany(options)
 }
 
-const getAssignmentById = async(id, select = null, include = null)=>{
-    if (!id || isNaN(id))
+const getAssignmentById = async(assignId, select = null, include = null)=>{
+    if (!assignId || isNaN(assignId))
         throw new Error('Invalid assignment ID');
-    const options = {where: {id: parseInt(id)}}
+    const options = {where: {id: parseInt(assignId)}}
     if(select)
         options.select = select
     if(include)
@@ -61,6 +72,6 @@ const isEnrolledAndActive = async (userId, classId) => {
   return enrollment?.status === 'Active'
 }
 
-module.exports = {getClasses, getClassById,
+module.exports = {getClasses, getClassById, getClassByOrgId, 
      getAssignments, getAssignmentById, getAssignmentByName,
       getEnrollment, isEnrolledAndActive}
